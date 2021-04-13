cf58dfc50475a83d988f58f63ac1e05146838bfd
Arielv1 <arielv1994@gmail.com>
2021-04-13 22:20:30 +0300

    Raycast drawer animation
    Raycast drawer animation

--- Assets/Scripts/DrawerMotion.cs
+++ Assets/Scripts/DrawerMotion.cs
@@ -6,24 +6,58 @@ public class DrawerMotion : MonoBehaviour
 {
     Animator animator;
     bool isDrawerClosed;
+    public GameObject Crosshair;
+    public GameObject CrosshairTouch;
+    public GameObject aCamera;
+    bool isTriggerHit;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = this.gameObject.transform.parent.GetComponent<Animator>();
         isDrawerClosed = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        //raycast forward from main camera
+        if (Physics.Raycast(aCamera.transform.position, aCamera.transform.forward, out hit))
+        {
+            if (hit.transform.gameObject.name == this.gameObject.name && hit.distance < 8)
+            {
+                if (!isTriggerHit)
+                {
+                    isTriggerHit = true;
+                    Crosshair.SetActive(false);
+                    CrosshairTouch.SetActive(true);
+                    
+                }
+
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    animator.SetBool("open", isDrawerClosed);
+                    isDrawerClosed = !isDrawerClosed;
+                }
+            }
+            else
+            {
+                if(isTriggerHit)
+                {
+                    isTriggerHit = false;
+                    Crosshair.SetActive(true);
+                    CrosshairTouch.SetActive(false);
+                }
+                
+            }
+        }
     }
 
-    private void OnMouseDown()
+    /*private void OnMouseDown()
     {
         animator.SetBool("open", isDrawerClosed);
         isDrawerClosed = !isDrawerClosed;
-        
-
-    }
+       
+    }*/
 }
